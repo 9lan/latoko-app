cdd4424a0b06eef1cf4ea5ea41beab7b3dbefdd8
Andang Vijayanto Utomo <andangvijayantoutomo@gmail.com>
2021-09-26 14:25:13 +0700

    init crud

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { MyContext } from "../common/types";
 import { Post } from "../entities/Post";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -10,4 +10,40 @@ export class PostResolver {
 	): Promise<Post[]> {
 		return em.find(Post, {})
 	}
+
+	@Query(() => Post, { nullable: true })
+	async getSinglePost(
+		@Arg('id') id: number,
+		@Ctx() { em }: MyContext
+	): Promise<Post | null> {
+		return em.findOne(Post, { id: id })
+	}
+
+	@Mutation(() => Post)
+	async createPost(
+		@Arg('title') title: string,
+		@Ctx() { em }: MyContext
+	): Promise<Post> {
+		const post = em.create(Post, { title });
+		await em.persistAndFlush(post)
+		return post
+	}
+
+	@Mutation(() => Post, { nullable: true })
+	async updatePost(
+		@Arg('id') id: number,
+		@Arg('title', () => String, { nullable: true }) title: string,
+		@Ctx() { em }: MyContext
+	): Promise<Post | null> {
+		const post = await em.findOne(Post, { id })
+		if (!post) {
+			return null
+		}
+		if (typeof title !== 'undefined') {
+			post.title = title
+			await em.persistAndFlush(post)
+		}
+		return post
+	}
+
 }
